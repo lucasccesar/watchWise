c4bd137ed8f353d453db95212269b269535a5ea3
Lucas Cassiano <lucasccesar05@gmail.com>
2024-02-20 01:27:26 -0300

    Revert "deploying"
    This reverts commit 60bec0041f5cc8be2f26f3476a28e3ba5a275ae1.

--- watchWise.js
+++ watchWise.js
@@ -38,7 +38,6 @@ const search = document.getElementById('search'),
 /* Search show */
 searchBtn.addEventListener('click', () => {
     search.classList.add('show-search');
-    document.getElementById('searchInput').focus();
 });
 
 /* Search hidden */
